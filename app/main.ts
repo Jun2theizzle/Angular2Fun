0170da4fa665a0c438b30dee1ff1d8886282685a
Johnny <jcheng26@illinois.edu>
2016-10-09 22:32:08 -0500

    set up done?

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Jun on 10/9/2016.
+ */
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {AppModule} from 'app.module'
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
