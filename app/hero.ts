f215bafe54e6d8f4f029dbea87d90ae6749e9927
Johnny <jcheng26@illinois.edu>
2016-10-09 23:31:10 -0500

    wow...

---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Jun on 10/9/2016.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,9 @@
 /**
  * Created by Jun on 10/9/2016.
  */
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {AppModule} from 'app.module'
+import { AppModule } from './app.module';
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
