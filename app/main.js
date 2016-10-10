ea618fa14951c8d9c006032279823486a8487d5b
Johnny <jcheng26@illinois.edu>
2016-10-09 23:56:36 -0500

    afdsaf

--- app/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by Jun on 10/9/2016.
- */
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
